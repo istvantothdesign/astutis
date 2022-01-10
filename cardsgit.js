4ad78e9922392ce40a9dfb60d38d200bbebe2447
istvantothdesign <69763751+istvantothdesign@users.noreply.github.com>
2022-01-10 18:31:55 +0000

    Update cardsgit.js

--- cardsgit.js
+++ cardsgit.js
@@ -7,21 +7,26 @@ function astCourseCards() {
   function changeCards() {
     skinLower = skin.toString().toLowerCase();
 
+    console.log("Start");
     switch (skinLower) {
       case "rospa":
         for (i = 0; i < astCourseCard.length; ++i) {
-          astCourseCard[i].style.background = "red";
+          astCourseCard[i].style.backgroundImage =
+            "url(`https://www.barton.co.uk/wp-content/uploads/sites/10/2019/07/ROSPA-logo.jpg`)";
         }
         break;
       case "meta":
         for (i = 0; i < astCourseCard.length; ++i) {
-          astCourseCard[i].style.background = "blue";
+          astCourseCard[i].style.backgroundImage =
+            "url('https://static.dezeen.com/uploads/2021/11/meta-facebook-rebranding-name-news_dezeen_2364_col_hero2.jpg')";
         }
         break;
       case "twitter":
         for (i = 0; i < astCourseCard.length; ++i) {
-          astCourseCard[i].style.background = "green";
+          astCourseCard[i].style.backgroundImage =
+            "url(https://1000logos.net/wp-content/uploads/2021/04/Twitter-Logo-2010.png)";
         }
+        console.log("Finish");
         break;
     }
   }
